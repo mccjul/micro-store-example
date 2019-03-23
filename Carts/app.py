140c5b4fb1adce9ad900240c6f093a9f7648daeb
Dethbug <mccjul@gmail.com>
2019-03-23 14:09:24 -0400

    Carts

--- Carts/app.py
+++ Carts/app.py
@@ -1,16 +1,34 @@
-from wsgiref.simple_server import make_server
-from pyramid.config import Configurator
-from pyramid.response import Response
+from flask import Flask, jsonify, request, abort
+from flask_redis import FlaskRedis
 
-def hello_world(request):
-    print('Request inbound!')
-    return Response('Docker works with Pyramid!')
+app = Flask(__name__)
+app.config['REDIS_URL'] = "redis://localhost:6379/0"
+redis_store = FlaskRedis(app)
+
+def add_to_cart(session, item, quantity=1):
+    return redis_store.hset('cart:' + session, item, quantity)
+
+def get_cart(session):
+    cart_items = []
+    b_cart = redis_store.hgetall('cart:' + session)
+    for item in b_cart.keys():
+        cart_items.append({"item": item.decode("utf-8"), "quantity": b_cart[item].decode("utf-8")})
+    return cart_items
+
+@app.route('/', methods=['POST', 'GET'])
+def items():
+    if request.method == 'POST':
+        if not request.json:
+            abort(400)
+        add_to_cart(request.json['session'], request.json['item'], request.json['quantity'])
+        return jsonify({'success':True}), 201
+
+    elif request.method == 'GET':
+        if request.args.get('id') is not None:
+            return jsonify(get_cart(request.args.get('id'))), 200
+
+        return abort(400)
 
 
 if __name__ == '__main__':
-    config = Configurator()
-    config.add_route('hello', '/')
-    config.add_view(hello_world, route_name='hello')
-    app = config.make_wsgi_app()
-    server = make_server('0.0.0.0', 6543, app)
-    server.serve_forever()
+    app.run(debug=True, port=5002)
--- Orders/app.py
+++ Orders/app.py
@@ -1,16 +1,57 @@
-from wsgiref.simple_server import make_server
-from pyramid.config import Configurator
-from pyramid.response import Response
+from flask import Flask, jsonify, request, abort
+from flask_sqlalchemy import SQLAlchemy
 
-def hello_world(request):
-    print('Request inbound!')
-    return Response('Docker works with Pyramid!')
+app = Flask(__name__)
+POSTGRES = {
+    'user': 'postgres',
+    'pw': 'password',
+    'db': 'test',
+    'host': 'localhost',
+    'port': '5432',
+}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://%(user)s:\
+%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+    id = db.Column(db.Integer, primary_key=True)
+    item_id = db.Column(db.String, unique=False)
+    customer = db.Column(db.String)
+    amount = db.Column(db.String, unique=False)
+
+    def __init__(self, item_id, customer, amount):
+        self.customer = customer
+        self.item_id = item_id
+        self.amount = amount
+
+    @property
+    def serialize(self):
+        """Return object data in easily serialized format"""
+        return {
+            'id': self.id,
+            'item_id': self.item_id,
+            'customer': self.customer,
+            'amount': self.amount,
+        }
+
+@app.route('/', methods=['POST', 'GET'])
+def items():
+    if request.method == 'POST':
+        if not request.json:
+            abort(400)
+
+        order = Order(request.json['item_id'], request.json['customer'], request.json['amount'])
+        db.session.add(order)
+        db.session.commit()
+        return jsonify(order.serialize), 201
+
+    elif request.method == 'GET':
+        query = Order.query.all()
+        return jsonify([item.serialize for item in query]), 200
 
 
 if __name__ == '__main__':
-    config = Configurator()
-    config.add_route('hello', '/')
-    config.add_view(hello_world, route_name='hello')
-    app = config.make_wsgi_app()
-    server = make_server('0.0.0.0', 6543, app)
-    server.serve_forever()
+    db.create_all()
+    app.run(debug=True, port=5001)
